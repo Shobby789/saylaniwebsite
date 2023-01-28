2005ba638f27b4c456a40a1d6ed846c52aa46f7c
Shobby789 <smshoaib2001@gmail.com>
2023-01-29 00:20:44 +0500

    I created a component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import SaylaniGuideForm from "../about/saylani-guide/SaylaniGuideForm";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import "./Contact.css";
 
 export default function Contact() {
   return (
-    <div className="contact-container">
+    <div>
       <Header heading="Contact us" />
+      <div className="contact-container"></div>
+      <SaylaniGuideForm />
       <Footer />
       <Outlet />
     </div>
