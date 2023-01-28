951404fcdd38367c8a05eae49aef596fa5f24579
Shobby789 <smshoaib2001@gmail.com>
2023-01-28 22:38:36 +0500

    I am making the components responsive.

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -13,7 +13,16 @@ export default function Courses() {
           mollitia.
         </p>
         <div className="courseList">
-          <div className="courseBox shadow p-3 bg-white rounded text-center">
+          <div className="box">
+            <img src={course1} alt="" />
+          </div>
+          <div className="box">
+            <img src={course2} alt="" />
+          </div>
+          <div className="box">
+            <img src={course3} alt="" />
+          </div>
+          {/* <div className="courseBox shadow p-3 bg-white rounded text-center">
             <img src={course1} alt="" />
             <h5>
               Web & Mobile <span className="app">App</span>
@@ -31,12 +40,20 @@ export default function Courses() {
             <img src={course3} alt="" />
             <h5>AI Chatbot</h5>
             <button className="btn btn-success">Enroll Now</button>
-          </div>
+          </div> */}
+          {/* <h5>shoaib muhammad</h5>
+          <h5>shoaib muhammad</h5>
+          <h5>shoaib muhammad</h5>
+          <h5>shoaib muhammad</h5>
+          <h5>shoaib muhammad</h5>
+          <h5>shoaib muhammad</h5> */}
+
+          {/*CourseList ends here  */}
         </div>
 
-        <button id="AllCoursesBtn" className="btn btn-success">
+        {/* <button id="AllCoursesBtn" className="btn btn-success">
           See All Courses
-        </button>
+        </button> */}
       </div>
     </>
   );
