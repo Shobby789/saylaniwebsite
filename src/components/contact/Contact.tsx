b7972c98551b8e9d66850f8244743bcf53196be2
Shobby789 <smshoaib2001@gmail.com>
2023-01-29 00:51:27 +0500

    I created a component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,13 +4,49 @@ import SaylaniGuideForm from "../about/saylani-guide/SaylaniGuideForm";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import "./Contact.css";
+import logo from "../images/saylani-logo-1.png";
 
 export default function Contact() {
   return (
     <div>
       <Header heading="Contact us" />
-      <div className="contact-container"></div>
-      <SaylaniGuideForm />
+      <div className="contact-container">
+        <img src={logo} alt="" />
+        <div className="form-container">
+          <form action="/action_page.php">
+            <label htmlFor="fname">First Name</label>
+            <input
+              type="text"
+              id="fname"
+              name="firstname"
+              placeholder="Your name.."
+            />
+            <label htmlFor="lname">Last Name</label>
+            <input
+              type="text"
+              id="lname"
+              name="lastname"
+              placeholder="Your last name.."
+            />
+            <label htmlFor="country">City</label>
+            <select id="city" name="city">
+              <option value="karachi">Karachi</option>
+              <option value="hyderbad">Hyderbad</option>
+              <option value="lahore">Lahore</option>
+              <option value="islamabad">Islamabad</option>
+            </select>
+            <label htmlFor="subject">Subject</label>
+            <textarea
+              id="subject"
+              name="subject"
+              placeholder="Write something.."
+              style={{ height: 200 }}
+              defaultValue={""}
+            />
+            <input type="submit" defaultValue="Submit" />
+          </form>
+        </div>
+      </div>
       <Footer />
       <Outlet />
     </div>
